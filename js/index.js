0d8ce0b437cafb9811239771f94cbcd06eab1f24
Cakes <kathryjmiranda@gmail.com>
2025-01-29 21:39:29 -0800

    API feth completed

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ document.body.appendChild(footer);
 //Skills
 const skills = ["Javascript", "HTML", "CSS", "Adobe Photoshop", "Github"];
 const skillsSection = document.querySelector("#skills");
-const skillsList= skillsSection.querySelector("ul");
+const skillsList = skillsSection.querySelector("ul");
 if (skillsSection) {
     skills.forEach(skill => {
         const skillItem = document.createElement("li");
@@ -20,3 +20,48 @@ if (skillsSection) {
 
 
 }
+
+
+// FETCH API
+const githubUsername = 'xpwet';
+fetch(`https://api.github.com/users/${githubUsername}/repos`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.text();
+    })
+
+    // Which is right
+    .then(repositories => {
+        const projectSection = document.getElementById('projects');
+        const projectList = projectSection.querySelector('.projects');
+        projectList.innerHTML ='';
+        repositories.forEach(repo => {
+            const project = document.createElement('li');
+            project.innerText = repo.name;
+            projectList.appendChild(project);
+        });
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+        const projectList = projectSection.querySelector('.project-list');
+        projectList.innerHTML = '<li> Sorry, something went wrong. Please try again later.</li>';
+    });
+
+// Is it this
+    then(text => {
+        const repositories = JSON.parse(text);
+        if (repositories.length === 0) {
+            console.log ('You have no repositories or the Projects section is empty.');
+        } else {
+           console.log(repositories);
+        repositories.forEach(repo => {
+            console.log(`Repo name: &{repo.name}, URL: ${repo.html_url}`);
+        });
+    }
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+        console.log('Sorry, something went wrong with fetching your repositories. Please try again later.');
+    });
